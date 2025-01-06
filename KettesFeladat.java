0a17bdb34d77c79a64b073fcfea5b7ca8789d45a
Blevente96 <levente.bozso22@gmail.com>
2025-01-06 09:51:58 +0100

    Feladat 2. teljesítve

---
+++ KettesFeladat.java
@@ -0,0 +1,29 @@
+public class KettesFeladat {
+    public static void main(String[] args) {
+
+        String word = "management";
+        boolean wordgotA = false;
+        char[] charArray = word.toCharArray();
+        int position = 0;
+
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == 'a') {
+                wordgotA = true;
+                position = i;
+                break;
+
+
+            }
+
+
+        }
+        if(wordgotA) {
+            System.out.println("The word got 'A' in it");
+            System.out.println("The first 'A' position is: "+ position);
+        }else System.out.println("The word got no 'A' in it");
+
+
+    }
+
+}
