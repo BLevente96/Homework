f2b54834c25e64c0f88242dde84290b00dd02158
Blevente96 <levente.bozso22@gmail.com>
2025-01-06 10:43:12 +0100

    Feladat 3. teljesítve

---
+++ HarmasFeladat.java
@@ -0,0 +1,21 @@
+public class HarmasFeladat {
+    public static void main(String[] args) {
+
+        String word = "flibbertigibbet";
+        char[] wordArray = word.toCharArray();
+        int countB = 0;
+
+
+        for (int i = 0; i < wordArray.length; i++) {
+            if (wordArray[i] == 'b') {
+                countB++;
+                System.out.println("'B' letter found, position: "+i);
+
+
+            }
+        }
+        System.out.println("The number of 'b's in the word is: "+countB);
+
+
+    }
+}
